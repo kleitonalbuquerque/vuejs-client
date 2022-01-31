c1ff278357c25bb040c8d1fdcc9d463d7860018f
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-31 12:33:30 -0300

    List all tutorials

--- src/services/TutorialDataService.js
+++ src/services/TutorialDataService.js
@@ -1,33 +1,33 @@
 import http from "../http-common";
 
-var tutorialDataService = class TutorialDataService {
+const TutorialDataService = {
   getAll() {
     return http.get("/tutorials");
-  }
+  },
 
   get(id) {
     return http.post(`/tutorials/${id}`);
-  }
+  },
 
   create(data) {
     return http.post("/tutorials", data);
-  }
+  },
 
   update(id, data) {
     return http.put(`/tutorials/${id}`, data);
-  }
+  },
 
   delete(id) {
     return http.delete(`/tutorials/${id}`);
-  }
+  },
 
   deleteAll() {
     return http.delete(`/tutorials`);
-  }
+  },
 
   findByTitle(title) {
     return http.get(`tutorials?title=${title}`);
-  }
+  },
 };
 
-export default tutorialDataService;
+export default TutorialDataService;
